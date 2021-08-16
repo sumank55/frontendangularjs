1f8c3eda1cab41cb2d8a3ef3fa8306c507c0fe58
suman <sumankumariideal@gmail.com>
2021-08-16 18:20:30 +0530

    fetch data

--- module.js
+++ module.js
@@ -1,68 +1,307 @@
 
-var app = angular.module("mainApp", [])
+
+var app = angular.module("myMod", ['ngRoute']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+
+    $routeProvider.
+        when("/add", {
+            templateUrl: 'views/add.html',
+            controller:'add_ctrl'
+        }).
+        when("/list", {
+            templateUrl: 'views/list.html',
+            controller:'list_ctrl'
+        }).
+        when("/delete", {
+            templateUrl: 'views/delete.html',
+            controller:'delete_ctrl'
+        }).
+        when("/edit", {
+            templateUrl: 'views/edit.html',
+            controller: 'edit_ctrl'
+            
+        }).otherwise({
+            redirectTo:'index.html'
+        })
+}])
+    
+    
+app.controller("list_ctrl", function ($scope,$http) {
+    $scope.employee = [];
+                
+    $http({
+        method: 'GET',
+        url: 'http://127.0.0.1:8000/employee/'
+    }).then(function (response) {
+        $scope.employee = response.data;
+    }, function (error) {
+        console.log(error,"can not get");
+
+    });
+})
+
+ 
+   
+
+
+var option = "json=" + encodeURI(JSON.stringify([
+    {
+        id: "",
+        first_name: "",
+        last_name: "",
+        email: "",
+        title_name: ""
+    
+    
+}
+
+]));    
+app.controller("add_ctrl", function ($scope, $http) {
+
+    $scope.employee = [];
+    $scope.AddData = function () {
+        $http({
+            method: 'POST',
+            url: 'http://127.0.0.1:8000/employee/',
+            data: option
+        }).then(function (data, status) {
+           
+            $scope.employee = data;
+            console.log("data",employee);
+            console.log("data added")
+        }, function (error) {
+
+            console.log(error, " You can not add");
+            
+        });
+    };
+        
+});
+  
+    app.controller("delete_ctrl", function ($scope) {
+      
+        
+        
+    });
+    
+    app.controller("edit_ctrl", function ($scope) {
+        
+        
+    });
+
+
+
+
+
+
+
+// var app = angular.module("myapp", ['ngRoute']);
+
+
+// app.config(["$routeProvider", function ($routeProvider) {
+
+
+//     $routeProvider.
+//         when("/add", {
+//             templateUrl: 'views/add.html',
+//             controller:'add_ctrl'
+//         }).
+//         when("/list", {
+//             templateUrl: 'views/list.html',
+//             controller:'list_ctrl'
+//         }).
+//         when("/delete", {
+//             templateUrl: 'views/delete.html',
+//             controller:'delete_ctrl'
+//         }).
+//         when("/edit", {
+//             templateUrl: 'views/edit.html',
+//             controller: 'edit_ctrl'
+            
+//         }).otherwise({
+//             redirectTo:'index.html'
+//         })
+// }])
+
+
+
+// app.controller("add_ctrl", function ($scope) {
+//         $scope.EmpList = [];
+//         $scope.AddData = function () {
+                
+//                 $http({
+//                         method : 'POST',
+//                         url :  'http://127.0.0.1:8000/employee/',
+//                         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+//                 }).success(function(data, status, headers, config) {
+//                         console.log("Success", status);
+//                 }).error(function(data, status, headers, config) {
+//                         console.log("Error " + data);
+                       
+//                 }); 
+        
+        
+             
+
+    
+//         }
+    
+// });
+
+
+// app.controller("list_ctrl", function ($scope) {
+        
+//         $scope.employee = [];
+//                 $http({
+//                         method: 'GET', url: 'http://127.0.0.1:8000/employee/',
+//                         headers: { 'X-Parse-Application-Id': 'XXX', 'X-Parse-REST-API-Key': 'YYY' }
+//                 })
+//                 .success(function(data, status) {
+//                         $scope.employee = data;
+//                 })
+//                         error(function(data, status) {
+//                         alert("Error");
+//                 });
+    
+// });
+
+
+
+// app.controller("delete_ctrl", function ($scope) {
+    
+    
+    
+// });
+
+// app.controller("edit_ctrl", function ($scope) {
+        
+// });
+// app.controller("maincontroller", function ($scope) {
+        
+// });
+
+
+
+
+
+
+
+
+ 
+// var app = angular.module("mainApp", [])
  
 
-app.controller("crudController", function ($scope) {
+// app.controller("crudController", function ($scope, $http) {
+// //     var myTestApp = angular.module("myTestModule", [])
+// // myTestApp.controller("customerController", function ($scope, $http) {
+//         var url = "http://localhost:8000/employee/";
+//         // fetch(url, {mode: "no-cors"})
+//         headers : {
 
-    $scope.EmpList = [];
-    $scope.AddData = function () {
-        var emp = {
-            Id: $scope.EmpList.length + 1,
-            First_Name: $scope.First_Name,
-            Last_Name: $scope.Last_Name,
-            Email: $scope.Email,
-            Role: $scope.Role
+            
+//             "Content-type" : "application/json"
+//             "Access-Control-Allow-Origin": "*"
+            
+//             }
+        
 
-
-        };
-
-        $scope.EmpList.push(emp);
-        ClearModel();
-    };  
-
-    //Delete the data 
-
-    $scope.DeleteData = function (emp) {
-        var index = $scope.EmpList.indexOf(emp);
-        $scope.EmpList.splice(index, 1);
-    };
+//         $http.post(url).then( function(response) {
+//            $scope.employees = response.data;
+//         });
+    
+// });
 
 
-    //function to bind data to the input fields
-
-    $scope.BindSelectedData = function (emp) {
 
 
-        $scope.Id = emp.Id;
-        $scope.First_Name = emp.First_Name;
-        $scope.Last_Name = emp.Last_Name;
-        $scope.Email = emp.Email;
-        $scope.Role= emp.Role;
-    }
 
 
-    //Update data for
-    $scope.UpdateData    = function () {
-        $.grep($scope.EmpList, function (e) {
-            if(e.id == $scope.Id)
-            {
-
-                e.first_Name = $scope.First_Name;
-                //e.Last_Name = $scope.Last_Name;
-                //e.Email = $scope.Email;
-                //e.Role=$scope.Role;
-            }
-        });
-    }
 
 
-    function ClearModel() {
-        $scope.Id = 0;
-        $scope.First_Name = '';
-        $scope.Last_Name= '';
-        $scope.Email = '';
 
-    }
+
+// var app = angular.module("mainApp", [])
+ 
+//     app.controller("crudController", function ($scope, $http) {
+//         //     var myTestApp = angular.module("myTestModule", [])
+//         // myTestApp.controller("customerController", function ($scope, $http) {
+//                 var url = "http://localhost:8000/employee/";
+                
+//                 // headers : {
+        
+                    
+//                 //     "Content-type" : "application/json"
+//                 //     "Access-Control-Allow-Origin": "*"
+                    
+//                 //     }
+                
+        
+//                 $http.post(url).then( function(response) {
+//                    $scope.employees = response.data;
+//                 });
+            
+//     });
+        
+//     $scope.EmpList = [];
+//     $scope.AddData = function () {
+//         var emp = {
+//             Id: $scope.EmpList.length + 1,
+//             First_Name: $scope.First_Name,
+//             Last_Name: $scope.Last_Name,
+//             Email: $scope.Email,
+//             Role: $scope.Role
+
+
+//         };
+
+//         $scope.EmpList.push(emp);
+//         ClearModel();
+//     };  
+
+//     //Delete the data 
+
+//     $scope.DeleteData = function (emp) {
+//         var index = $scope.EmpList.indexOf(emp);
+//         $scope.EmpList.splice(index, 1);
+//     };
+
+
+//     //function to bind data to the input fields
+
+//     $scope.BindSelectedData = function (emp) {
+
+
+//         $scope.Id = emp.Id;
+//         $scope.First_Name = emp.First_Name;
+//         $scope.Last_Name = emp.Last_Name;
+//         $scope.Email = emp.Email;
+//         $scope.Role= emp.Role;
+//     }
+
+
+//     //Update data for
+//     $scope.UpdateData    = function () {
+//         $.grep($scope.EmpList, function (e) {
+//             if(e.id == $scope.Id)
+//             {
+
+//                 e.first_Name = $scope.First_Name;
+//                 //e.Last_Name = $scope.Last_Name;
+//                 //e.Email = $scope.Email;
+//                 //e.Role=$scope.Role;
+//             }
+//         });
+//     }
+
+
+//     function ClearModel() {
+//         $scope.Id = 0;
+//         $scope.First_Name = '';
+//         $scope.Last_Name= '';
+//         $scope.Email = '';
+
+//     }
 
      
-});
+// });
